f79ca87daa39b1ffa0047cf99aa47e320f4660d5
Ángel Sánchez <mektigboy@protonmail.com>
2022-05-24 15:03:18 -0500

    Update CustomError.sol

--- CustomError.sol
+++ CustomError.sol
@@ -4,9 +4,11 @@ pragma solidity ^ 0.8.14;
 contract CustomError {
   address payable owner = payable(msg.sender);
 
+  error Unauthorized(address caller);
+
   function withdraw() public {
     if (msg.sender != owner) {
-        revert("Error.");
+        revert Unauthorized(msg.sender);
     }
 
     owner.transfer(address(this).balance);
